bc443ad5a3b900c4ced8b1191f8d554909e00c33
James Rogers <jimrogers3315@gmail.com>
2022-02-20 07:28:52 -0600

    finish

---
+++ ApiMiniProject/Controllers/AddressController.cs
@@ -0,0 +1,26 @@
+﻿using ApiMiniProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ApiMiniProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressController : ControllerBase
+    {
+        private ILogger _logger;
+
+        public AddressController(ILogger<AddressController> logger)
+        {
+            _logger = logger;
+        }
+
+        // POST api/Address
+        [HttpPost]
+        public void Post([FromBody] AddressModel data)
+        {
+            _logger.LogInformation("The address was logged as {Address}", data);
+        }
+    }
+}
